4fd1d4e7a343655ea3e52fa21c17f85e46027b51
MuriloPCoelho <murilopc14@hotmail.com>
2025-10-01 11:35:56 -0300

    feat: add database seeding script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -5,6 +6,6 @@ export default defineConfig({
   out: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
