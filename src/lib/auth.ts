9c89c70e54fb2579b77ce34050357e00c1d31a6f
MuriloPCoelho <murilopc14@hotmail.com>
2025-10-03 17:51:14 -0300

    chore: Termina configuração do better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,12 @@
+import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
-  database: drizzleAdapter({
-    user: "tb_users",
-    account: "tb_accounts",
-    session: "tb_sessions",
-    verification: "tb_verifications",
-  }, {
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
     provider: "pg",
   }),
 });
