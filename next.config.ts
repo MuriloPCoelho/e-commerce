e12f30e6bcc022af2bafdf9692ac8dd47374b712
MuriloPCoelho <murilopc14@hotmail.com>
2025-10-14 20:23:23 -0300

    chore: Configura caminho para as imagens

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://d4lgxe9bm8juw.cloudfront.net/products/**')]
+  }
 };
 
 export default nextConfig;
