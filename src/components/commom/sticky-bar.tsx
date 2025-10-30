c7a1125f82686fea9de1344c2ffa1aaeb46ed307
MuriloPCoelho <murilopc14@hotmail.com>
2025-10-30 00:06:50 -0300

    feat: Adiciona componente StickyBar para dispor informações gerais para o usuário

---
+++ src/components/commom/sticky-bar.tsx
@@ -0,0 +1,10 @@
+const StickyBar = () => {
+  return ( 
+    <div className="h-10 bg-neutral-200 flex justify-center items-center text-xs gap-1">
+      <span>Frete grátis a partir de </span>
+      <b>R$599,00</b>
+    </div>
+   );
+}
+ 
+export default StickyBar;
