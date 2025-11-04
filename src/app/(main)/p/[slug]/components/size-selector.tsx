ca5d4446c1c336990a4ef91c5c6a0a2c5090e240
MuriloPCoelho <murilopc14@hotmail.com>
2025-11-04 09:11:08 -0300

    refactor: Simplifica a importação e ajusta estilos no componente SizeSelector

--- src/app/(main)/p/[slug]/components/size-selector.tsx
+++ src/app/(main)/p/[slug]/components/size-selector.tsx
@@ -1,11 +1,9 @@
 "use client";
-import {
-  productVariantSizesTable,
-  sizesTable,
-} from "@/db/schema";
+import { productVariantSizesTable, sizesTable } from "@/db/schema";
 import Link from "next/link";
 import { useSearchParams, useParams } from "next/navigation";
 import { twJoin } from "tailwind-merge";
+import { buttonVariants } from "@/components/ui/button";
 
 interface SizeSelectorProps {
   sizes: (typeof productVariantSizesTable.$inferSelect & {
@@ -25,12 +23,14 @@ const SizeSelector = ({ sizes }: SizeSelectorProps) => {
           scroll={false}
           href={`/p/${params.slug}?size=${size.size.name}`}
           className={twJoin(
-            "outline outline-neutral-300 size-12 aspect-square flex items-center justify-center",
-            size.size.name === sizeParam && "ring-2 ring-amber-400"
+            "outline outline-neutral-300 w-12 h-8 rounded-xs flex items-center justify-center font-semibold text-sm",
+            size.size.name === sizeParam && "bg-black text-white",
+            size.stock <= 0 && "bg-neutral-100 text-neutral-400 relative"
           )}
           key={size.id}
         >
           {size.size.name}
+          {size.stock <= 0 && <div className="w-14 absolute h-[1px] bg-neutral-300 -rotate-[33.69deg]"></div>}
         </Link>
       ))}
     </div>
