8be61f01cde3e9c641352268210e16296573d5da
MuriloPCoelho <murilopc14@hotmail.com>
2025-11-04 09:12:45 -0300

    fix: Corrige a cor de fundo e texto no componente StickyBar

--- src/components/commom/sticky-bar.tsx
+++ src/components/commom/sticky-bar.tsx
@@ -1,6 +1,6 @@
 const StickyBar = () => {
   return ( 
-    <div className="h-10 bg-neutral-200 flex justify-center items-center text-xs gap-1">
+    <div className="h-10 bg-black text-white flex justify-center items-center text-xs gap-1">
       <span>Frete grátis a partir de </span>
       <b>R$599,00</b>
     </div>
