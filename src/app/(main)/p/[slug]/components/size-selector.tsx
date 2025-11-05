986ade5f9934f0ac37faec6e30fd008320771746
MuriloPCoelho <murilopc14@hotmail.com>
2025-11-05 18:00:40 -0300

    refactor: Remove unused import and improve className logic in SizeSelector component

--- src/app/(main)/p/[slug]/components/size-selector.tsx
+++ src/app/(main)/p/[slug]/components/size-selector.tsx
@@ -3,7 +3,6 @@ import { productVariantSizesTable, sizesTable } from "@/db/schema";
 import Link from "next/link";
 import { useSearchParams, useParams } from "next/navigation";
 import { twJoin } from "tailwind-merge";
-import { buttonVariants } from "@/components/ui/button";
 
 interface SizeSelectorProps {
   sizes: (typeof productVariantSizesTable.$inferSelect & {
@@ -23,14 +22,27 @@ const SizeSelector = ({ sizes }: SizeSelectorProps) => {
           scroll={false}
           href={`/p/${params.slug}?size=${size.size.name}`}
           className={twJoin(
-            "outline outline-neutral-300 w-12 h-8 rounded-xs flex items-center justify-center font-semibold text-sm",
-            size.size.name === sizeParam && "bg-black text-white",
-            size.stock <= 0 && "bg-neutral-100 text-neutral-400 relative"
+            "outline outline-neutral-300 w-12 h-8 rounded-xs flex items-center justify-center font-semibold text-sm overflow-hidden",
+            size.size.name === sizeParam &&
+              "bg-black text-white outline-black!",
+            size.stock <= 0 && "bg-neutral-100 relative",
+            size.size.name === sizeParam &&
+              size.stock <= 0 &&
+              "bg-neutral-400 outline-neutral-500!"
           )}
           key={size.id}
         >
           {size.size.name}
-          {size.stock <= 0 && <div className="w-14 absolute h-[1px] bg-neutral-300 -rotate-[33.69deg]"></div>}
+          {size.stock <= 0 && (
+            <div
+              className={twJoin(
+                "w-16 absolute h-[1px] bg-neutral-300 -rotate-[33.69deg]",
+                size.size.name === sizeParam &&
+                  size.stock <= 0 &&
+                  "bg-neutral-500"
+              )}
+            ></div>
+          )}
         </Link>
       ))}
     </div>
