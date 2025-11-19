a2bd5addbc2ca5a96521d6d2a06c8f746f93884e
MuriloPCoelho <murilopc14@hotmail.com>
2025-11-18 23:29:28 -0300

    feat: remove botão de location

--- src/components/commom/header.tsx
+++ src/components/commom/header.tsx
@@ -1,6 +1,4 @@
 import { getMenus } from "@/actions/get-menus";
-import { MapPin } from "lucide-react";
-import { Button } from "../ui/button";
 import Bag from "./bag";
 import NavigationDrawer from "./navigation-drawer";
 import { UserDropdown } from "./user-dropdown";
@@ -11,11 +9,8 @@ const Header = async () => {
   return (
     <header className="pb-16 relative z-30">
       <div className="w-full flex items-center h-16 justify-between px-4 py-3 border-b border-neutral-100 fixed bg-white" >
-        <div className="flex items-center gap-1">
+        <div>
           <NavigationDrawer menus={menus} />
-          <Button variant="ghost" title="Location">
-            <MapPin className="size-6" />
-          </Button>
         </div>
         <div className="flex items-center gap-1">
           <UserDropdown />
